b7aec025f7bb541be5532ec819356764052953a6
Blond <vsmokx@gmail.com>
2023-04-25 11:46:07 +0200

    🛠 Bootstrap

--- pages/login.php
+++ pages/login.php
@@ -1,28 +1,85 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MyCar</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <link rel="stylesheet" href="../assets/css/style.css">
+  <title>MyCar</title>
 </head>
+
 <body>
-    <header>
+  <header class="w-100">
+    <nav class="navbar navbar-dark bg-dark">
+      <div class="container-fluid">
+        <img class="" style="width:90px; height:90px;" src="../assets/img/icon.png" alt="">
+        <a class="navbar-brand text-danger" href="#">MyCar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title text-danger" id="offcanvasDarkNavbarLabel">Menu</h5>
+            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <div class="nav-item">
+                <button class="btn btn-danger"><a href="./pages/login.php">login</a></button>
+              </div>
+              <div class="nav-item">
+                <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" aria-current="page" href="#">Account</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown
+                  </a>
+                  <ul class="dropdown-menu dropdown-menu-dark">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li>
+                      <hr class="dropdown-divider">
+                    </li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                  </ul>
+                </li>
+            </ul>
+            <form class="d-flex mt-3" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-danger" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </nav>
 
-    </header>
 
-    <main>
-    <form class="form" action="../controller/login.php" method="post">
+  </header>
+  <main>
+    <div class="container">
+      <form class="form" action="../controller/login.php" method="post">
         <div class="email">
-        <label for="email">email</label>
-        <input type="email" name="email" id="email">
+          <label for="email">email</label>
+          <input type="email" name="email" id="email">
         </div>
         <div class="pwd">
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password">
+          <label for="password">Password</label>
+          <input type="password" name="password" id="password">
         </div>
         <button type="submit">Log In</button>
-    </form>
-    </main>
+      </form>
+    </div>
+
+  </main>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
+
 </html>
