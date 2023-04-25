0ac48450da81ca47a179d32620eebe06b08782dd
Blond <vsmokx@gmail.com>
2023-04-25 11:43:58 +0200

    🛠 Bootstrap

--- index.php
+++ index.php
@@ -5,38 +5,97 @@ session_start();
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <link rel="stylesheet" href="./assets/css/style.css">
+  <title>Document</title>
 </head>
 
 <body>
-    <p>bonjour <?= $_SESSION['userName']; ?></p>
-    <h1>fosbf</h1>
-    <form action="./controller/clear_session.php" method="POST">
-        <input type="submit" value="Clear session" />
+  <header class="w-100">
+    <nav class="navbar navbar-dark bg-dark">
+      <div class="container-fluid">
+        <img class="" style="width:90px; height:90px;" src="./assets/img/icon.png" alt="">
+        <a class="navbar-brand text-danger" href="#">MyCar</a>
+        <div class="d-flex align-items-baseline">
+          <h6 class='text-danger me-3'>Hello <?= !empty($_SESSION['userName']) ? $_SESSION['userName'] : 'Invité' ?></h6>
+          <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+        <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title text-danger" id="offcanvasDarkNavbarLabel">Menu</h5>
+            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <div class="nav-item">
+                <li>
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" aria-current="page" href="#">Account</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown
+                  </a>
+                  <ul class="dropdown-menu dropdown-menu-dark">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li>
+                      <hr class="dropdown-divider">
+                    </li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                  </ul>
+                </li>
+            </ul>
+            <div class="">
+              <button class="btn btn-danger"><a href="./pages/login.php">login</a></button>
+              <button class="btn btn-danger"><a href="./pages/register.php">Register</a></button>
+            </div>
+            <form class="d-flex mt-3" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-danger" type="submit">Search</button>
+            </form>
+            <?= !empty($_SESSION['userId']) ? '<form class="d-flex justify-content-end mt-5" action="./controller/disconect.php" method="POST">
+              <a href="./controller/disconect.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-right me-2" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z" />
+                  <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
+                </svg>Disconnect</a>
+            </form>' : ' '?>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <main>
+    <form action="./controller/img.php" method="post" enctype="multipart/form-data">
+      <label for="file">import</label>
+      <input type="file" name='file'>
+      <button type="submit">envoyer</button>
     </form>
-    <a href="./pages/login.php">login</a>
-    <a href="./pages/register.php">Register</a>
-    <div>
-        <form action="./controller/img.php" method="post" enctype="multipart/form-data">
-            <label for="file">import</label>
-            <input type="file" name='file'>
-            <button type="submit">envoyer</button>
-        </form>
     </div>
     <div>
-        <?php
+      <?php
+      if (!empty($_SESSION['userId'])) {
         $img_filter = array_filter(json_decode(file_get_contents('./database/img.json')), function ($img) {
-            return $img->userId == $_SESSION['userId'];
+          return $img->userId == $_SESSION['userId'];
         });
         foreach ($img_filter as $value) {
-            echo "<img src='./assets/img/uploads/" . $value->name . "'alt='images'>";
-        } ?>
+          echo "<img src='./assets/img/uploads/" . $value->name . "'alt='images'>";
+        }
+      }
+      ?>
     </div>
-
-
+  </main>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 
 </html>
