a815c0011610dc1d0ed4ab71fb3e0f4553b6c0c7
Blond <vsmokx@gmail.com>
2023-04-28 07:59:45 +0200

    🛠 New Form Login an Register

--- pages/login.php
+++ pages/login.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,19 +9,23 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-  <link rel="stylesheet" href="../assets/css/style.css">
-  <title>MyCar</title>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+  <link rel="stylesheet" href="../assets/css/login.css">
+  <title>Document</title>
 </head>
 
-<body>
+<body class="img js-fullheight" style="background-image: url('https://cdn.wallpapersafari.com/22/27/vJNp6z.jpg'); height: 710px;">
   <header class="w-100">
     <nav class="navbar navbar-dark bg-dark">
       <div class="container-fluid">
         <img class="" style="width:90px; height:90px;" src="../assets/img/icon.png" alt="">
-        <a class="navbar-brand text-danger" href="#">MyCar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+        <a class="navbar-brand text-danger" href="../index.php">MyCar</a>
+        <div class="d-flex align-items-baseline">
+          <h6 class='text-danger me-3'>Hello <?= !empty($_SESSION['userName']) ? $_SESSION['userName'] : 'Invité' ?></h6>
+          <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
         <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
           <div class="offcanvas-header">
             <h5 class="offcanvas-title text-danger" id="offcanvasDarkNavbarLabel">Menu</h5>
@@ -27,10 +34,8 @@
           <div class="offcanvas-body">
             <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
               <div class="nav-item">
-                <button class="btn btn-danger"><a href="./pages/login.php">login</a></button>
-              </div>
-              <div class="nav-item">
-                <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
+                <li>
+                  <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link" aria-current="page" href="#">Account</a>
@@ -52,19 +57,27 @@
                   </ul>
                 </li>
             </ul>
+            <div>
+              <button class="btn btn-danger"><a href="./login.php">login</a></button>
+              <button class="btn btn-danger"><a href="./register.php">Register</a></button>
+            </div>
             <form class="d-flex mt-3" role="search">
               <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
               <button class="btn btn-danger" type="submit">Search</button>
             </form>
+            <?= !empty($_SESSION['userId']) ? '<form class="d-flex justify-content-end mt-5" action="../controller/disconect.php" method="POST">
+              <a href="../controller/disconect.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-right me-2" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z" />
+                  <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
+                </svg>Disconnect</a>
+            </form>' : ' ' ?>
           </div>
         </div>
       </div>
     </nav>
-
-
   </header>
   <main>
-    <div class="container">
+    <!-- <div class="container">
       <form class="form" action="../controller/login.php" method="post">
         <div class="email">
           <label for="email">email</label>
@@ -76,8 +89,31 @@
         </div>
         <button type="submit">Log In</button>
       </form>
-    </div>
+    </div> -->
 
+    <!-- Pills content -->
+    <div class="wrapper">
+        <div class="logo">
+            <img src="../assets/img/User_Avatar_2.png" alt="">
+        </div>
+        <div class="text-center mt-4 name">
+            Login MyCar
+        </div>
+        <form class="p-3 mt-3" action="../controller/login.php" method="post">
+            <div class="form-field d-flex align-items-center">
+                <span class="far fa-user"></span>
+                <input type="email" name="email" id="userName" placeholder="Email">
+            </div>
+            <div class="form-field d-flex align-items-center">
+                <span class="fas fa-key"></span>
+                <input type="password" name="password" id="pwd" placeholder="Password">
+            </div>
+            <button class="btn mt-3">Login</button>
+        </form>
+        <div class="text-center">
+        <p>You dont have account :</p><a href="./register.php">Sign up</a>
+        </div>
+    </div>
   </main>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
